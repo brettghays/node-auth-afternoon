4599f9f665fbe37a8a8550bd51e02187997027af
Brett Hays <brett.g.hays.coding@gmail.com>
2018-02-13 22:30:08 -0700

    mostly done with auth afternoon

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
 const express = require('express');
 const session = require('express-session');
+const passport = require('passport');
+const strategy = require('./strategy');
+const request = require('request')
 
 const app = express();
 app.use( session({
@@ -8,5 +11,67 @@ app.use( session({
   saveUninitialized: false
 }));
 
+app.use(passport.initialize());
+app.use(passport.session());
+passport.use(strategy);
+
+passport.serializeUser(function(user,done){
+  console.log(user)
+  const { _json } = user;
+  done(null, {
+    clientID: _json.clientID, 
+    email: _json.email, 
+    name: _json.name, 
+    followers_url: _json.followers_url 
+  })
+});
+
+passport.deserializeUser(function(obj,done){
+  done(null,obj)
+})
+
+app.get('/login', passport.authenticate('auth0',{
+  successRedirect: '/followers',
+  failureRedirect: '/login',
+  failureFlash: true,
+  connection: 'github'
+}));
+
+app.get('/followers', (req,res,next) => {
+  if(!req.user){
+    res.redirect('/login')
+  } else {
+    let options = {
+      url: req.user.followers,
+      headers: {
+        'User-Agent': req.user.clientID
+      }
+    };
+
+    let callback = (error, response, body) => {
+      res.status(200).send(body)
+    };
+
+    request(options,callback)
+  }
+})
+
+/* app.get('/followers', ( req, res, next ) => {
+  if ( req.user ) {
+    const FollowersRequest = {
+      url: req.user.followers,
+      headers: {
+        'User-Agent': req.user.clientID
+      }
+    };
+
+    request(FollowersRequest, ( error, response, body ) => {
+      console.log(res.status(200).send(body));
+    });
+  } else {
+    res.redirect('/login');
+  }
+});
+ */
 const port = 3000;
 app.listen( port, () => { console.log(`Server listening on port ${port}`); } );
